71dc50ca1a7cfaa7ea7ecb84fe9114d94aa52da5
Guls <Gulslabs@users.noreply.github.com>
2024-02-27 21:50:39 +0530

    Update SenderReceiver.sol

--- 2-sending-ether/examples/0-send-ether/src/SenderReceiver.sol
+++ 2-sending-ether/examples/0-send-ether/src/SenderReceiver.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.0;
 contract Sender {
     // Function to send value to another contract
     function sendValue(address payable recipient) external payable {
-        // Use call function to send Ether to the recipient contract
+        // OPTION 1:  Use call function to send Ether to the recipient contract
         (bool success, ) = recipient.call{value: msg.value}("");
         require(success, "Transfer failed");
+        // OPTION 2:  Use transfer function to send Ether to the recipient contract
+        // recipient.transfer(msg.value);
+        
     }
 }
 
